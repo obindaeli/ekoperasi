d8f4dafb46c82e08cfb9aa714e994474e56668bf
devkominfonisbar@gmail.com <devkominfonisbar@gmail.com>
2023-08-15 15:26:19 +0700

    ganti css

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -17,7 +17,7 @@
   <link rel="stylesheet" href="<?=base_url()?>/assets/modules/datatables/datatables.min.css">
   <link rel="stylesheet" href="<?=base_url()?>/assets/modules/datatables/DataTables-1.10.16/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="<?=base_url()?>/assets/modules/datatables/Select-1.2.4/css/select.bootstrap4.min.css">
-
+  
   <!-- Template CSS -->
   <link rel="stylesheet" href="<?=base_url()?>/assets/css/style.css">
   <link rel="stylesheet" href="<?=base_url()?>/assets/css/components.css">
